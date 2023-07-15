39845136e8f0fa9370a84edac3ba2d1e3792b587
thenoxwitch <131623738+thenoxwitch@users.noreply.github.com>
2023-07-15 16:22:07 +0100

    changed units

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+//geolocation
+function showPosition(position) {
+  let p = document.querySelector(".geolocation-current");
+  p.innerHTML = `Your Latitude is ${position.coords.latitude} and your longitude is ${position.coords.longitude}`;
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", getCurrentPosition);
+
 //date
 let now = new Date();
 let h2 = document.querySelector("h2");
@@ -16,16 +29,6 @@ let minutes = now.getMinutes();
 
 h2.innerHTML = `${day} ${hours}:${minutes}`;
 
-//search form
-function search(event) {
-  event.preventDefault();
-
-  let searchInput = document.querySelector("#search-text-input");
-  searchCity(searchInput.value);
-}
-let form = document.querySelector("#search-city-form");
-form.addEventListener("submit", search);
-
 //WEATHERAPI
 
 function displayWeather(response) {
@@ -40,6 +43,9 @@ function displayWeather(response) {
   let conditions = document.querySelector(".conditions");
   let humidity = response.data.main.humidity;
   let iconElement = document.querySelector("#icon");
+
+  let celsiusTemperature = response.data.main.temp;
+
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -51,22 +57,47 @@ function displayWeather(response) {
 }
 
 function searchCity(city) {
-  let units = "metric";
   let apiKey = "a5acb752426cd8188485c35694980e3a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeather);
 }
 
-//geolocation api
-function showPosition(position) {
-  let p = document.querySelector(".geolocation-current");
-  p.innerHTML = `Your Latitude is ${position.coords.latitude} and your longitude is ${position.coords.longitude}`;
+//search form
+function search(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-text-input");
+  searchCity(searchInput.value);
+}
+//units
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function getCurrentPosition() {
-  navigator.geolocation.getCurrentPosition(showPosition);
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let button = document.querySelector("button");
-button.addEventListener("click", getCurrentPosition);
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-city-form");
+form.addEventListener("submit", search);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("London");
